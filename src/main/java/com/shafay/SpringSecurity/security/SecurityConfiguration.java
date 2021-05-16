010d38ee958e652bdfe03e8c3ca3262e21297dc7
shafayk <user7@sunrichtech.com>
2021-05-16 12:22:32 +0530

    Jdbc_Auth_With_Default_Schema
    1. create spring security example using jdbc auth default schema.
    
    Author- Khan Shafay

--- src/main/java/com/shafay/SpringSecurity/security/SecurityConfiguration.java
+++ src/main/java/com/shafay/SpringSecurity/security/SecurityConfiguration.java
@@ -1,31 +1,38 @@
 package com.shafay.SpringSecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  DataSource dataSource;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-            .antMatchers("/", "index", "/css/*", "/js/*").permitAll() // disable security urls
-            .antMatchers("/api/v1/employees").hasAnyRole("USER", "ADMIN")
+            .antMatchers("/", "index", "/css/*", "/js/*").permitAll() // disable urls
+            .antMatchers("/api/v1/employees/**").hasAnyRole("USER", "ADMIN")
             .antMatchers("/**").hasRole("ADMIN")
             .and().formLogin();
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-            .withUser("user")
-            .password("{noop}user")
-            .roles("USER")
-            .and()
-            .withUser("admin")
-            .password("{noop}admin")
-            .roles("ADMIN");
+    auth.jdbcAuthentication().dataSource(dataSource);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
