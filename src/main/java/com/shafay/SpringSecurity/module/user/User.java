e7e0d52f06b890f2b6ba36962f19ce9c73c57198
shafayk <user7@sunrichtech.com>
2024-05-11 13:10:55 +0530

    security-jdbc-form-based-authentication
    1. data jdbc form base authentication.
    
    Author- Khan Shafay

--- src/main/java/com/shafay/SpringSecurity/module/user/User.java
+++ src/main/java/com/shafay/SpringSecurity/module/user/User.java
@@ -4,25 +4,17 @@ import com.shafay.SpringSecurity.common.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class User {
+
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String password;
   private String role;
-  @Enumerated(EnumType.STRING)
   private Status status;
 }
--- src/main/java/com/shafay/SpringSecurity/module/user/UserRepository.java
+++ src/main/java/com/shafay/SpringSecurity/module/user/UserRepository.java
@@ -1,13 +1,13 @@
 package com.shafay.SpringSecurity.module.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
   List<User> findAll();
 
