2431b1e2deea6e77d90f9d5b739b6f32c94a46e8
shafayk <user7@sunrichtech.com>
2024-05-11 12:57:53 +0530

    master
    1. version reverted.
    
    Author- Khan Shafay

--- src/main/java/com/shafay/SpringSecurity/security/SecurityConfiguration.java
+++ src/main/java/com/shafay/SpringSecurity/security/SecurityConfiguration.java
@@ -1,22 +1,17 @@
 package com.shafay.SpringSecurity.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests
-            .requestMatchers("/", "index", "/css/*", "/js/*").permitAll() // disable security urls
-            .anyRequest().authenticated()).httpBasic(withDefaults());
-      return http.build();
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/", "index", "/css/*", "/js/*").permitAll() // disable security urls
+        .anyRequest().authenticated()
+        .and().httpBasic();
   }
 }
